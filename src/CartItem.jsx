5c3b3814950d9316536a7d25d1eb7003828af3e9
Cody Edwards <edwards.cody51@yahoo.com>
2026-08-20 14:46:56 -0400

    implemented CartItems component

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,40 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach((item) => {
+        const itemTotal = parseFloat(item.cost.substring(1)) * item.quantity;
+        total += itemTotal;
+    })
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    e.preventDefault();
+    onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  }
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ ...item, quantity:item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+   if (item.quantity > 1) {
+        dispatch(updateQuantity({ ...item, quantity:item.quantity - 1 }));
+   }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    parseFloat(item.cost.substring(1)) * item.quantity;
   };
 
   return (
